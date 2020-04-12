813931efa7d504d21bffee91baac295789934eb9
mike98989 <mike98989@gmail.com>
2020-04-12 14:28:34 +0100

    minor update

--- src/estimator.js
+++ src/estimator.js
@@ -1,10 +1,9 @@
 const covid19ImpactEstimator = (data) => {
-	const input=data;
-	return {
-		data:input,
-		impact:{},
-		serverImpact:{}
-	};
+    const input=data;
+    return {
+	    data:input,
+	    impact:{},
+	    serverImpact:{}
+   };
 };
-
 export default covid19ImpactEstimator;
