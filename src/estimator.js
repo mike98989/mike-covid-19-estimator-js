226e9779a3466147f27564d3835c19456a8def74
mike98989 <mike98989@gmail.com>
2020-04-12 16:12:23 +0100

    challanges

--- src/estimator.js
+++ src/estimator.js
@@ -1,9 +1,41 @@
+/* eslint-disable max-len */
 const covid19ImpactEstimator = (data) => {
   const input = data;
+  // CHALLENGE 1
+  const impactData = {};
+  const severeImpactData = {};
+
+  // Impact Data
+  impactData.currentlyInfected = input.currentlyInfected * 10;
+  impactData.infectionsByRequestedTime = impactData.currentlyInfected * 2 ** 10;
+
+  // Severe Impact Data
+  severeImpactData.currentlyInfected = input.currentlyInfected * 50;
+  severeImpactData.infectionsByRequestedTime = severeImpactData.currentlyInfected * 2 ** 10;
+
+  // CHALLENGE 2
+  // Impact Data
+  impactData.severeCasesByRequestedTime = impactData.infectionsByRequestedTime * (15 / 100);
+  impactData.hospitalBedsByRequestedTime = data.hospitalBedsByRequestedTime - impactData.severeCasesByRequestedTime;
+
+  // Severe Impact Data
+  severeImpactData.severeCasesByRequestedTime = severeImpactData.infectionsByRequestedTime * (15 / 100);
+  severeImpactData.hospitalBedsByRequestedTime = data.hospitalBedsByRequestedTime - severeImpactData.severeCasesByRequestedTime;
+
+  // CHALLENGE 3
+  // Impact Data
+  impactData.casesForICUByRequestedTime = impactData.infectionsByRequestedTime * (5 / 100);
+  impactData.casesForVentilatorsByRequestedTime = impactData.infectionsByRequestedTime * (2 / 100);
+  impactData.dollarsInFlight = (impactData.infectionsByRequestedTime * input.region.avgDailyIncomePopulation) * input.region.avgDailyIncomeInUSD * 30;
+  // Severe Impact Data
+  severeImpactData.casesForICUByRequestedTime = severeImpactData.infectionsByRequestedTime * (5 / 100);
+  severeImpactData.casesForVentilatorsByRequestedTime = severeImpactData.infectionsByRequestedTime * (2 / 100);
+  severeImpactData.dollarsInFlight = (severeImpactData.infectionsByRequestedTime * input.region.avgDailyIncomePopulation) * input.region.avgDailyIncomeInUSD * 30;
+
   return {
     data: input,
-    impact: {},
-    serverImpact: {}
+    impact: impactData,
+    serverImpact: severeImpactData
   };
 };
 export default covid19ImpactEstimator;
