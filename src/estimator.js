a4cd0fca1d71ab290c0f378bda0acb617f7de9fb
mike98989 <mike98989@gmail.com>
2020-04-12 13:05:35 +0100

    First Commit

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,11 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+	const inputData=data,
+	return{
+		data:inputData,
+		impact:{},
+		serverImpact:{}
+	}
+
+}
 
 export default covid19ImpactEstimator;
